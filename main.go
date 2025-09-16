9029fdaf32e1a49c0293545a8e52375503a5e409
Manny-14 <okorieemmanuelapp@gmail.com>
2025-09-16 09:24:07 -0500

    add main.go to parse logs into CSV
    - CLI takes input log and output CSV paths
    - Parse lines with parser.ParseLogLine and write records
    - Log warnings for malformed lines
    - Move parser files into parser/ directory

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"log"
+	"os"
+	"strconv"
+
+	logpb "github.com/Manny-14/LAD/gen/proto/log"
+	"github.com/Manny-14/LAD/parser"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		log.Fatal("Error: Please provide an input log file path and an output CSV file path.")
+	}
+	inputPath := os.Args[1]
+	outputPath := os.Args[2]
+
+	// Open input file
+	inputFile, err := os.Open(inputPath)
+	if err != nil {
+		log.Fatal("Error opening input file: %w", err)
+	}
+	defer inputFile.Close()
+
+	// Create output file
+	outputFile, err := os.Create(outputPath)
+	if err != nil {
+		log.Fatal("Error creating output CSV file: %w", err)
+	}
+	defer outputFile.Close()
+
+	csvWriter := csv.NewWriter(outputFile)
+	defer csvWriter.Flush()
+
+	header := []string{"timestamp", "pid", "severity", "component", "message"}
+	if err := csvWriter.Write(header); err != nil {
+		log.Fatalf("Error writing CSV header: %w", err)
+	}
+
+	// process log file
+	scanner := bufio.NewScanner(inputFile)
+	for scanner.Scan() {
+		line := scanner.Text()
+		logEntry, err := parser.ParseLogLine(line)
+		if err != nil {
+			// Log errors to stderr instead of stdout
+			log.Printf("Warning: Skipping malformed line: %q (%w)\n", line, err)
+			continue
+		}
+
+		record := logEntryToRecord(logEntry)
+		if err := csvWriter.Write(record); err != nil {
+			log.Printf("Warning: Could not write record to CSV: %w", err)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error reading from file: %w", err)
+	}
+
+	log.Printf("Successfully processed log file and saved structured data to %s\n", outputPath)
+}
+
+func logEntryToRecord(entry *logpb.LogEntry) []string {
+	return []string{
+		strconv.Itoa(int(entry.Timestamp)),
+		strconv.Itoa(int(entry.Pid)),
+		entry.Severity.String(),
+		entry.Component,
+		entry.Message,
+	}
+}
